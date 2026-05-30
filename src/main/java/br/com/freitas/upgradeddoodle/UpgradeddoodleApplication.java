3f328941937884913954d88ec3c0c04e7fd6d037
Matheus Freitas <mf022rj@gmail.com>
2026-05-29 22:53:09 -0300

    Initial commit

---
+++ src/main/java/br/com/freitas/upgradeddoodle/UpgradeddoodleApplication.java
@@ -0,0 +1,13 @@
+package br.com.freitas.upgradeddoodle;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UpgradeddoodleApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(UpgradeddoodleApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/freitas/upgradeddoodle/UpgradeddoodleApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.freitas.upgradeddoodle;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UpgradeddoodleApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
